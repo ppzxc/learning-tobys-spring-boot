906e70b4957ad53822789d9f39b497892bf5d590
ppzxc <cjh8487@naver.com>
2023-12-18 23:47:19 +0900

    2023-12-18 섹션3 - 프론트 컨트롤러

--- src/main/java/ppzxc/learning/LearningTobysSpringBootApplication.java
+++ src/main/java/ppzxc/learning/LearningTobysSpringBootApplication.java
@@ -7,21 +7,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 public class LearningTobysSpringBootApplication {
 
   public static void main(String[] args) {
     TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-    WebServer webServer = serverFactory.getWebServer(servletContext -> {
-      servletContext.addServlet("hello", new HttpServlet() {
+    WebServer webServer = serverFactory.getWebServer(
+      servletContext -> servletContext.addServlet("hello", new HttpServlet() {
         @Override
         protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-          resp.setStatus(200);
-          resp.setHeader("Content-Type", "text/plain");
-          resp.getWriter().print("Hello Servlet");
+          String name = req.getParameter("name");
+
+          resp.setStatus(HttpStatus.OK.value());
+          resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+          resp.getWriter().print("Hello %s".formatted(name));
         }
-      }).addMapping("/hello");
-    });
+      }).addMapping("/hello"));
     webServer.start();
   }
 }
