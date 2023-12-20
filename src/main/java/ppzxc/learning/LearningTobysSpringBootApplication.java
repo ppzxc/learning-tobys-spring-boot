4d59e076077a28694d6980b31fa5e44df4c1560d
ppzxc <cjh8487@naver.com>
2023-12-20 23:08:52 +0900

    2023-12-19 섹션4 - 자바 코드 구성 정보 사용

--- src/main/java/ppzxc/learning/LearningTobysSpringBootApplication.java
+++ src/main/java/ppzxc/learning/LearningTobysSpringBootApplication.java
@@ -2,15 +2,29 @@ package ppzxc.learning;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 import ppzxc.learning.controller.HelloController;
+import ppzxc.learning.service.HelloService;
 import ppzxc.learning.service.SimpleHelloService;
 
+@Configuration
 public class LearningTobysSpringBootApplication {
 
+  @Bean
+  public HelloController helloController(HelloService helloService) {
+    return new HelloController(helloService);
+  }
+
+  @Bean
+  public HelloService helloService() {
+    return new SimpleHelloService();
+  }
+
   public static void main(String[] args) {
-    GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+    AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
       @Override
       protected void onRefresh() {
         super.onRefresh();
@@ -23,8 +37,7 @@ public class LearningTobysSpringBootApplication {
         webServer.start();
       }
     };
-    applicationContext.registerBean(SimpleHelloService.class);
-    applicationContext.registerBean(HelloController.class);
+    applicationContext.register(LearningTobysSpringBootApplication.class);
     applicationContext.refresh();
   }
 }
