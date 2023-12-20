324a6c6fdfd246c2e8500e06c1be8329ada563fb
ppzxc <cjh8487@naver.com>
2023-12-20 21:30:03 +0900

    2023-12-19 섹션4 - 애노테이션 매핑 정보 사용

--- src/main/java/ppzxc/learning/LearningTobysSpringBootApplication.java
+++ src/main/java/ppzxc/learning/LearningTobysSpringBootApplication.java
@@ -1,15 +1,9 @@
 package ppzxc.learning;
 
-import java.io.IOException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 import ppzxc.learning.controller.HelloController;
 import ppzxc.learning.service.SimpleHelloService;
 
@@ -17,7 +11,7 @@ public class LearningTobysSpringBootApplication {
 
   public static void main(String[] args) {
     // Spring ApplicationContext
-    GenericApplicationContext applicationContext = new GenericApplicationContext();
+    GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
     applicationContext.registerBean(SimpleHelloService.class);
     applicationContext.registerBean(HelloController.class);
     applicationContext.refresh();
@@ -25,22 +19,8 @@ public class LearningTobysSpringBootApplication {
     // Servlet Container
     TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
     WebServer webServer = serverFactory.getWebServer(
-      servletContext -> servletContext.addServlet("frontcontroller", new HttpServlet() {
-        @Override
-        protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-          if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-            String name = req.getParameter("name");
-
-            HelloController helloController = applicationContext.getBean(HelloController.class);
-            String ret = helloController.hello(name);
-
-            resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-            resp.getWriter().print(ret);
-          } else {
-            resp.setStatus(HttpStatus.NOT_FOUND.value());
-          }
-        }
-      }).addMapping("/*"));
+      servletContext -> servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext))
+        .addMapping("/*"));
     webServer.start();
   }
 }
