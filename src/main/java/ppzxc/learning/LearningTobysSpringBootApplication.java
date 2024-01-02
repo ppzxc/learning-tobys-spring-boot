18a5fdeae8c57974a3f85db9e8379710a61143a5
ppzxc <cjh8487@naver.com>
2024-01-02 22:30:18 +0900

    2024-01-02 섹션6 - 합성 애노테이션의 적용

--- src/main/java/ppzxc/learning/LearningTobysSpringBootApplication.java
+++ src/main/java/ppzxc/learning/LearningTobysSpringBootApplication.java
@@ -1,26 +1,10 @@
 package ppzxc.learning;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
+import ppzxc.learning.annotation.MySpringBootAnnotation;
 
-@Configuration
-@ComponentScan
+@MySpringBootAnnotation
 public class LearningTobysSpringBootApplication {
-
-  @Bean
-  public ServletWebServerFactory servletWebServerFactory() {
-    return new TomcatServletWebServerFactory();
-  }
-
-  @Bean
-  public DispatcherServlet dispatcherServlet() {
-    return new DispatcherServlet();
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(LearningTobysSpringBootApplication.class, args);
