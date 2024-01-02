d9e6befe5f815c0d1f3e2104eae2aff2c74625ba
ppzxc <cjh8487@naver.com>
2024-01-02 21:40:59 +0900

    2024-01-02 섹션5 - 테스트 코드를 이용한 테스트

---
+++ src/test/java/ppzxc/learning/HelloApiTest.java
@@ -0,0 +1,36 @@
+package ppzxc.learning;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+public class HelloApiTest {
+
+  private TestRestTemplate rest;
+
+  @BeforeEach
+  void setUp() {
+    rest = new TestRestTemplate();
+  }
+
+  @Test
+  void should_return_hello_world() {
+    // given
+    String given = "Spring";
+
+    // when
+    ResponseEntity<String> actual = rest.getForEntity("http://localhost:8080/hello?name={name}", String.class,
+      given);
+
+    // then
+    assertThat(actual.getStatusCode()).isEqualByComparingTo(HttpStatus.OK);
+    assertThat(actual.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+    assertThat(actual.getBody()).isEqualTo("Hello %s".formatted(given));
+  }
+}
