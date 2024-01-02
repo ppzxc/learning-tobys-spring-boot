6d8d0b226ff44167aae4e52c409b76e256f924c0
ppzxc <cjh8487@naver.com>
2024-01-02 23:38:52 +0900

    2024-01-02 섹션6 - 자동 구성 정보 파일 분리

--- src/main/java/ppzxc/configuration/MyAutoConfigurationImportSelector.java
+++ src/main/java/ppzxc/configuration/MyAutoConfigurationImportSelector.java
@@ -1,15 +1,21 @@
 package ppzxc.configuration;
 
+import java.util.stream.StreamSupport;
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
 public class MyAutoConfigurationImportSelector implements DeferredImportSelector {
 
+  private final ClassLoader classLoader;
+
+  public MyAutoConfigurationImportSelector(ClassLoader classLoader) {
+    this.classLoader = classLoader;
+  }
+
   @Override
   public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-    return new String[]{
-      "ppzxc.configuration.auto.DispatcherServletConfiguration",
-      "ppzxc.configuration.auto.TomcatWebServerConfiguration",
-    };
+    return StreamSupport.stream(ImportCandidates.load(MyAutoConfiguration.class, classLoader).spliterator(), false)
+      .toArray(String[]::new);
   }
 }
