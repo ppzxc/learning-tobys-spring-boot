5940f4e91e287e5ac38a2c3e3fa1b86550e49438
ppzxc <cjh8487@naver.com>
2024-01-11 00:02:18 +0900

    2024-01-04 섹션9 - DataSource 자동 구성 클래스

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework:spring-jdbc")
 //    implementation("com.h2database:h2:2.2.220")
-    implementation("com.h2database:h2:2.1.214")
+    runtimeOnly("com.h2database:h2:2.1.214")
 //    implementation("hikari-cp:hikari-cp:3.0.1")
     implementation("com.zaxxer:HikariCP:5.1.0")
     // Use Jetty
--- src/main/java/ppzxc/configuration/auto/DataSourceConfiguration.java
+++ src/main/java/ppzxc/configuration/auto/DataSourceConfiguration.java
@@ -1,7 +1,9 @@
 package ppzxc.configuration.auto;
 
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Driver;
 import javax.sql.DataSource;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import ppzxc.configuration.ConditionalMyOnClass;
@@ -14,17 +16,26 @@ import ppzxc.configuration.MyAutoConfiguration;
 public class DataSourceConfiguration {
 
   @Bean
+  @ConditionalMyOnClass("com.zaxxer.hikari.HikariDataSource")
+  @ConditionalOnMissingBean
+  public DataSource hikariDataSource(MyDataSourceProperties myDataSourceProperties) {
+    HikariDataSource dataSource = new HikariDataSource();
+    dataSource.setDriverClassName(myDataSourceProperties.getDriverClassName());
+    dataSource.setJdbcUrl(myDataSourceProperties.getUrl());
+    dataSource.setUsername(myDataSourceProperties.getUsername());
+    dataSource.setPassword(myDataSourceProperties.getPassword());
+    return dataSource;
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
   public DataSource dataSource(MyDataSourceProperties myDataSourceProperties) throws ClassNotFoundException {
     SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
     dataSource.setDriverClass((Class<? extends Driver>) Class.forName(
       myDataSourceProperties.getDriverClassName()));
     dataSource.setUrl(myDataSourceProperties.getUrl());
-    dataSource.setUrl(myDataSourceProperties.getUsername());
+    dataSource.setUsername(myDataSourceProperties.getUsername());
     dataSource.setPassword(myDataSourceProperties.getPassword());
     return dataSource;
   }
-
-//  @Bean
-//  public DataSource hikariDataSource(MyDataSourceProperties myDataSourceProperties) {
-//  }
 }
